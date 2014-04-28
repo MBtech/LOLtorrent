f898353b2e536e5c01d4b29401b6ec720c78ac67
MBTech <mbilal_ce@live.com>
2014-04-28 15:44:24 +0200

    Windows friendly

--- src/filesharing/test/Testing.java
+++ src/filesharing/test/Testing.java
@@ -18,24 +18,24 @@ public class Testing {
 	public static void main (String [] args) throws IOException, InterruptedException, ClassNotFoundException {
 		
 		// spawn a tracker
-		TrackerDaemon t = new TrackerDaemon("/tmp/t", "T1"); // XXX
+		TrackerDaemon t = new TrackerDaemon("./tmp/t", "T1"); // XXX
 		t.setLogging(false);
 		t.start();
 		
 		// seeder 1 seeds file
-		FileClient s1 = new FileClient("/tmp/c1", "S1"); // XXX
+		FileClient s1 = new FileClient("./tmp/c1", "S1"); // XXX
 		s1.setLogging(false);
 		s1.addTracker("localhost", TrackerDaemon.DEFAULT_TRACKER_PORT);
 		s1.seedFile("bigfile", FileTransfer.DEFAULT_BLOCK_SIZE); // XXX
 		
 		// seeder 2 seeds file
-		FileClient s2 = new FileClient("/tmp/c1", "S2"); // XXX
+		FileClient s2 = new FileClient("./tmp/c1", "S2"); // XXX
 		s2.setLogging(false);
 		s2.addTracker("localhost", TrackerDaemon.DEFAULT_TRACKER_PORT);
 		s2.seedFile("bigfile", FileTransfer.DEFAULT_BLOCK_SIZE); // XXX
 
 		// client 1 downloads and seeds the file
-		FileClient c1 = new FileClient("/tmp/c2", "C1");
+		FileClient c1 = new FileClient("./tmp/c2", "C1");
 		
 		// load client state from previous session
 		try {
