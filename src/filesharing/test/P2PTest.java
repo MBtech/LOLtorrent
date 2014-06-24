7dd6ce9d82eb9bc45eaf4c484eb5c64aaa193924
MBTech <mbilal_ce@live.com>
2014-06-24 18:57:44 +0200

    Updated the test file to allow for providing ip address of the tracker and the file size as input arguments

--- src/filesharing/test/P2PTest.java
+++ src/filesharing/test/P2PTest.java
@@ -1,6 +1,8 @@
 package filesharing.test;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 import org.apache.commons.io.FileUtils;
 
@@ -17,23 +19,56 @@ import filesharing.exception.NoMetadataException;
  *
  */
 public class P2PTest {
+	
+	private static void createFile(String filePath, int fileSize) throws IOException {
+		File f = new File(filePath);
+		ObjectOutputStream Oos;
+		//Check if the file exists. If not, create it
+		if(!f.exists())
+		{
+			Oos = new ObjectOutputStream(new FileOutputStream(f));
+			for (int i=0; i< fileSize*16384; i++)
+			{
+				Oos.write("This is a test file and this is a test line to file up 64 bytes.".getBytes());
+			}
+			Oos.close();
+		}
+	}
 
 	public static void main (String [] args) throws IOException, InterruptedException, ClassNotFoundException {
 		int nargs = args.length;
-		String ptype;
+		String ptype, trackerIP = "127.0.0.1";
+		int fileSize = 32;
 		if (nargs<1)
 		{
 			System.out.println("Please provide more arguments");
-			System.out.println("Usage: Program PeerType");
+			System.out.println("Usage: Program PeerType Tracker_IP[Optional] File_Size[Optional]");
 			return;
 		}
 		//Convert input arguments from string to integers
 		ptype = args[0];
+		if (ptype.compareTo("tracker")!=0){
+			if (nargs<2)
+			{
+			System.out.println("Please provide Tracker IP");
+			System.out.println("Usage: Program PeerType Tracker_IP[Optional] File_Size[Optional]");
+			return;
+			}
+			else
+			{
+				// Extract the trackerIP from input arguments
+				trackerIP = args[1];
+			}
+		}
+		if (nargs>2){
+			fileSize = Integer.parseInt(args[2]);
+		}
 		File tmpdir = new File(new String("./tmp"));
 		tmpdir.mkdir();
 		if (ptype.compareTo("tracker")==0){
 			File dir = new File(new String("./tmp/t"));
 			dir.mkdir();
+			createFile("./tmp/t/bigfile_"+fileSize,fileSize);
 			// spawn a tracker with the specified tracker ID and working directory
 			TrackerDaemon t = new TrackerDaemon("./tmp/t", "T1"); // XXX
 			t.setLogging(true);
@@ -43,10 +78,11 @@ public class P2PTest {
 			// Instantiate required amount of seeders 
 			File dir = new File(new String("./tmp/c1"));
 			dir.mkdir();
+			createFile("./tmp/c1/bigfile_"+fileSize, fileSize);
 			FileClient s = new FileClient("./tmp/c1", new String("S")); // XXX
 			s.setLogging(true);
-			s.addTracker("209.208.109.252", TrackerDaemon.DEFAULT_TRACKER_PORT);
-			s.seedFile("bigfile", FileTransfer.DEFAULT_BLOCK_SIZE); // XXX
+			s.addTracker(trackerIP, TrackerDaemon.DEFAULT_TRACKER_PORT);
+			s.seedFile("bigfile_"+fileSize, FileTransfer.DEFAULT_BLOCK_SIZE); // XXX
 		}
 		else if (ptype.compareTo("leecher")==0){
 			File dir = new File(new String("./tmp/c"));
@@ -61,13 +97,13 @@ public class P2PTest {
 			catch(IOException | NoMetadataException e){/*File not found*/}
 			
 			// add tracker
-			c1.addTracker("209.208.109.252", TrackerDaemon.DEFAULT_TRACKER_PORT);
+			c1.addTracker(trackerIP, TrackerDaemon.DEFAULT_TRACKER_PORT);
 
 			// try to download and then seed the file
 			while(true) {
 				try {
-					c1.downloadFile("bigfile"); // XXX
-					c1.seedFile("bigfile", FileTransfer.DEFAULT_BLOCK_SIZE);
+					c1.downloadFile("bigfile_"+fileSize); // XXX
+					c1.seedFile("bigfile_"+fileSize, FileTransfer.DEFAULT_BLOCK_SIZE);
 					break;
 				}
 				catch(NoMetadataException e) {
@@ -81,6 +117,7 @@ public class P2PTest {
 		else
 		{
 			System.out.println("Incorrect node type. Node type can only be: torrent, seeder or leecher");
+			System.exit(-1);
 		}
 
 		// force quit of the application
